e6576e6ce492c8be7c2810dbfd2e57465758f7e0
HjuCave <115939073+HjuCave@users.noreply.github.com>
2022-12-02 16:49:49 +0300

    02/12/22

--- LABVAR5/Program5.cs
+++ LABVAR5/Program5.cs
@@ -3,9 +3,9 @@
 
 namespace LABVAR5
 {
-    internal class Classic
-    {
-		protected int[] mv = new int [5];
+	internal class Classic
+	{
+		protected int[] mv = new int[5];
 		public int this[int j]
 		{
 			get
@@ -20,32 +20,41 @@ namespace LABVAR5
 		public float sum;
 		public float sred;
 		public float sumread
-        {
+		{
 			get { return sum; }
-        }
-		
-		public float sredread
-        {
-            get { return sred; }
-        }
+		}
 
-        static void Main(string[] args)
+		public float sredread
+		{
+			get { return sred; }
+		}
+	}
+	public class Counting
+    {
+		public void Count()
         {
 			Classic obj = new Classic();
-			
 			for (int i = 0; i < 5; i++)
-            {
+			{
 				Console.WriteLine("Введите элемент массива под индексом {0}", i);
-                obj[i] = int.Parse(Console.ReadLine());
-            }
+				obj[i] = int.Parse(Console.ReadLine());
+			}
 			for (int i = 0; i < 5; i++)
-            {
+			{
 				obj.sum = obj.sumread + obj[i];
-            }
-			obj.sred = obj.sum/5;
-			Console.WriteLine("Сумма = {0}",obj.sumread);
-			Console.WriteLine("Среднее арифметическое = {0}",obj.sredread);
+			}
+			obj.sred = obj.sum / 5;
+			Console.WriteLine("Сумма = {0}", obj.sumread);
+			Console.WriteLine("Среднее арифметическое = {0}", obj.sredread);
 			Console.ReadKey();
+		}
+    }
+	class Class
+	{ 
+        static void Main(string[] args)
+        {
+			Counting count = new Counting();
+			count.Count();
         }
     }
 }
